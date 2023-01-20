5b51db9c55b95858385451ac1c4768787855bc70
Tristan Voiron <tristan.voiron@wefight.co>
2023-01-20 11:33:18 +0100

    Update test

--- App.tsx
+++ App.tsx
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet, View,
+  TextInput,
+  Button,
+  Text,
+} from 'react-native';
 
-export default function App() {
+const App = () => {
+  
+  const [x, setX] = useState('');
+  const [y, setY] = useState('');
+  const [z, setZ] = useState('');
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <TextInput 
+        style={styles.input}
+        onChangeText={text => setX(text)}
+        value={x}></TextInput>
+    <TextInput 
+      style={styles.input}
+      onChangeText={text => { setY(text) }}
+      value={y}></TextInput>
+
+
+    <View style={styles.buttonsView}>
+      <Button
+        onPress={() => {setZ(parseInt(x)+parseInt(y))}}
+        title="+"
+        />
+
+    <Button
+      onPress={() => setZ(parseInt(x)-parseInt(y))}
+      title="-"
+      />
+          <Button
+            onPress={() => {setZ(parseInt(x)*parseInt(y))}}
+            title="*"
+            style={{}}
+            />
+        </View>
+    <Text style={styles.result}>{z}
+  </Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 12,
+    color: 'black',
+  },
+  buttonsView: {
+    margin: 12,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  result: {
+    margin: 12,
+    alignSelf: 'center',
+  }
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,21 @@
+# Wefight React Native Calculator Test
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm install`
+Install the libraries
+You can also install any library you would need using 
+### `npm install library-name`
+
+### `npm start`
+Runs the app in the development mode.
+
+The page will reload if you make edits.
+
+## Important Files
+
+You should just need to work on 2 of the files:
+`App.tsx`: is where you will find the code
+All the other files are basic config or generated, and are not part of the exercise 
